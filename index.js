a0196af21d3daf3b9050e8a0d748db1b66286164
Anderson LM <anderson_lopez@apple.com>
2017-01-13 12:15:13 -0500

    merge

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const PORT = process.env.PORT || 8080;
 const CLARIFAI = {
     id: process.env.CLARIFAI_ID || '' ,
     secret: process.env.CLARIFAI_SECRET || ''
-}
+};
 
 var express = require('express');
 var clarifai = require('./clarifai/clarifai')(CLARIFAI.id,CLARIFAI.secret); 
@@ -11,8 +11,18 @@ var app = express();
 
 app.get('/', function (req, res) {
     res.send('Hello World!')
-})
+});
+
+
+app.post('/api/v1/mail', function (req, res) {
+    // Sent email
+
+
+
+    res.send('Email Sent');
+
+});
 
 app.listen(PORT, function () {
     console.log('Example app listening on port ' + PORT)
-})
+});
