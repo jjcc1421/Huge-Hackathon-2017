14604ab9724a99c89704c0e39a10dd202e77ed93
anderson lopez <anderson1564@gmail.com>
2017-01-13 18:26:28 +0000

    merge

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
-const PORT = process.env.PORT || 8080;
+"use strict";
+
 const CLARIFAI = {
     id: process.env.CLARIFAI_ID || '',
     secret: process.env.CLARIFAI_SECRET || ''
 };
+const MAIL_DATA = { 
+    apiKey: process.env.MAILGUN_API_KEY, 
+    domain: process.env.MAILGUN_DOMAIN 
+};
+const Express = require('express');
+// const Mail = require('./src/mail');
 
-var express = require('express');
-var clarifai = require('./src/clarifai/clarifai')(CLARIFAI.id, CLARIFAI.secret);
-var app = express();
-var image = require('./src/image/image')();
-
+let clarifai = require('./src/clarifai/clarifai')(CLARIFAI.id, CLARIFAI.secret);
+let app = Express();
+let image = require('./src/image/image')();
 
 app.get('/', function (req, res) {
     res.send('Hello World!');
 });
+
 app.post('/image', function (req, res) {
     var base64 = req.body.base64;
     image.decode(base64);
@@ -20,12 +26,23 @@ app.post('/image', function (req, res) {
 })
 
 app.post('/api/v1/mail', function (req, res) {
-    // Sent email
+    // let mail = Mail(MAIL_DATA);
+    
+    // var data = {
+    //   from: 'Excited User <me@samples.mailgun.org>',
+    //   to: 'anderson1564@gmail.com',
+    //   subject: 'Hello',
+    //   text: 'Testing some Mailgun awesomness!'
+    // };
+
+    // mail.messages().send(data, function (error, body) {
+    //   console.log(body);
+    // });
 
     res.send('Email Sent');
 
 });
 
-app.listen(PORT, function () {
-    console.log('Example app listening on port ' + PORT)
+app.listen(process.env.PORT || 8080, process.env.IP || "0.0.0.0", function (){
+  console.log("listening");
 });
---
+++ src/mail.js
@@ -0,0 +1,5 @@
+"use strict";
+
+const MailGun = require('mailgun-js');
+
+module.exports = (options) => MailGun(options);
