7b664ad718efb424c566fc62c2016d83255b388f
Anderson LM <anderson_lopez@apple.com>
2017-01-13 12:25:12 -0500

    Fixing merge

--- index.js
+++ index.js
@@ -13,20 +13,13 @@ app.get('/', function (req, res) {
     res.send('Hello World!')
 });
 
-<<<<<<< HEAD
-
 app.post('/api/v1/mail', function (req, res) {
     // Sent email
-
-
 
     res.send('Email Sent');
 
 });
 
-=======
->>>>>>> d3e57b408ffa294a9b63e272e7e2b69e6ede1da1
-app.post
 app.listen(PORT, function () {
     console.log('Example app listening on port ' + PORT)
 });
