7ebce8602e3ab92035545e63d0c2ad24f350dc9b
anderson lopez <anderson1564@gmail.com>
2017-01-13 20:55:24 +0000

    Small fixes in anaytics

--- src/analytics.js
+++ src/analytics.js
@@ -3,11 +3,10 @@
 const ua = require('universal-analytics');
 
 module.exports = function (trackingId) {
-    let module = {};
-    module.event = (eventCategory, eventActions) => {
-        let visitor = ua(trackingId);
-        visitor.event(eventCategory, eventActions).send();
-    }
-
-    return module;
+    return {
+        event: (eventCategory, eventActions) => {
+            let visitor = ua(trackingId);
+            visitor.event(eventCategory, eventActions).send();
+        }
+    };
 };
