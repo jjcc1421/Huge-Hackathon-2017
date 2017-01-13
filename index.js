48c063efb0c05c6239ee2a5b4ce337dd68720a7b
Juan Caicedo <jcaicedo@hugeinc.com>
2017-01-13 12:44:25 -0500

    Include base 64 image

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 const PORT = process.env.PORT || 8080;
 const CLARIFAI = {
-    id: process.env.CLARIFAI_ID || '' ,
+    id: process.env.CLARIFAI_ID || '',
     secret: process.env.CLARIFAI_SECRET || ''
 }
 
 var express = require('express');
-var clarifai = require('./clarifai/clarifai')(CLARIFAI.id,CLARIFAI.secret); 
+var clarifai = require('./clarifai/clarifai')(CLARIFAI.id, CLARIFAI.secret);
 var app = express();
+var image = require('./image/image')();
 
 
 app.get('/', function (req, res) {
-    res.send('Hello World!')
+    res.send('Hello World!');
+});
+app.post('/image', function (req, res) {
+    var base64 = req.body.base64;
+    image.decode(base64);
+    res.send('Hello World!');
 })
 
-app.post
 app.listen(PORT, function () {
     console.log('Example app listening on port ' + PORT)
 })
