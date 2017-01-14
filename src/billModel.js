ce235ff0bd261250d6648f0d797e1351ec8f1bda
Juan Caicedo <jcaicedo@hugeinc.com>
2017-01-14 01:29:07 -0500

    Concept value

--- src/billModel.js
+++ src/billModel.js
@@ -10,18 +10,19 @@ module.exports = function () {
                 value: null,
                 currency: null
             };
-            switch (concept.name) {
-                case 'Mil Pesos':
-                    parsed.value = 1000;
-                    parsed.currency = 'COP'
-                    break;
-                case 'Cinco Mil Pesos':
-                    parsed.value = 5000;
-                    parsed.currency = 'COP'
-                    break;
-                default:
-                    break;
-            }
+            if (concept.value > 0.6)
+                switch (concept.name) {
+                    case 'Mil Pesos':
+                        parsed.value = 1000;
+                        parsed.currency = 'COP'
+                        break;
+                    case 'Cinco Mil Pesos':
+                        parsed.value = 5000;
+                        parsed.currency = 'COP'
+                        break;
+                    default:
+                        break;
+                }
             return parsed;
         }
     };
