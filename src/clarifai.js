13783a01b1eef51597ea1d0a99cec3fb6cd59291
Juan Caicedo <jcaicedo@hugeinc.com>
2017-01-13 23:36:24 -0500

    Get model clarifai

--- src/clarifai.js
+++ src/clarifai.js
@@ -7,23 +7,10 @@ module.exports = function (clientId, clientSecret, clientModelId) {
 
     return {
         predict: (imageUrl, callBack) => {
-            app.models.get(clientModelId).then(
-                function (model) {
-                    model.predict(imageUrl).then(
-                        (response) => { callBack(response) },
-                        (err) => { console.error('Error: ', err) }
-                    );
-                },
-                function (err) {
-                    // there was an error
-                }
-            );
-
-
-            /*app.models.predict(clientModelId, imageUrl).then(
+            app.models.predict(imageUrl).then(
                 (response) => { callBack(response) },
                 (err) => { console.error('Error: ', err) }
-            );*/
+            );
         }
     };
 };
